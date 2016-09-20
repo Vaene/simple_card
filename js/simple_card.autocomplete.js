894431c49aa6f1c8f2dc17584566b594665b124d
Randy Howk <randy@greatist.com>
2016-09-20 10:46:29 -0400

    simple_card: first commit.

---
+++ js/simple_card.autocomplete.js
@@ -0,0 +1,20 @@
+// Autosubmit on keyboard & click.
+Drupal.jsAC.prototype.hidePopup = function (keycode) {
+  // Select item if the right key or mousebutton was pressed.
+  if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+    this.input.value = $(this.selected).data('autocompleteValue');
+    if (jQuery(this.input).hasClass('auto-submit')) {
+      this.input.form.submit();
+    }
+  }
+  // Hide popup.
+  var popup = this.popup;
+  if (popup) {
+    this.popup = null;
+    $(popup).fadeOut('fast', function () { $(popup).remove(); });
+  }
+  this.selected = false;
+  $(this.ariaLive).empty();
+};/**
+ * Created by randyhowk on 6/27/16.
+ */
